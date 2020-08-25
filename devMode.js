d9dcb372e3d0564c004a3805249bbcceed109237
MediumLowQuality <>
2020-08-25 12:11:53 -0400

    Create devMode.js
    running this file creates a Muninn instance that only responds to Victor in the console, preventing duplicate command execution in other cases.

---
+++ devMode.js
@@ -0,0 +1,21 @@
+const {loggedIn, handleMessage} = require('./main');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.commands = new Discord.Collection();
+bot.commandAliases = [];
+const botCommands = require('./commands');
+Object.keys(botCommands).map(key => {
+	bot.commands.set(botCommands[key].name, botCommands[key]);
+	if(botCommands[key].alias !== undefined) bot.commandAliases.push(botCommands[key].alias);
+});
+
+const TOKEN = process.env.TOKEN;
+delete process.env.TOKEN;
+process.bot = bot;
+
+bot.login(TOKEN);
+
+bot.on('ready', loggedIn);
+
+bot.on('message', (msg) => if(msg.author.id === process.env.ADMIN && msg.channel.name === 'console') handleMessage(msg));
